5461df450d3f096e7b2fc59cf8a82ba6a4e0063f
spirolad <spiroles@laposte.net>
2022-06-02 14:11:28 +0200

    Update files

--- app.js
+++ app.js
@@ -10,12 +10,14 @@ app.use(upload());
 app.use(express.static('public'));
 
 // Init server and database
-fs.readFile('', 'utf8', function(err, data) {
+fs.readFile('./privacy/conf.json', 'utf8', function(err, data) {
     if(err){
         console.error(err);
         return;
     }
-    var dbURI = data;
+    var conf = JSON.parse(data);
+    var databaseId = conf.nosql;
+    var dbURI = 'mongodb://'+ nosql.username +':'+ nosql.password +'@'+ databaseId.address +'/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.5.0';
     mongoose.connect(dbURI)
         .then((result) => {
             app.listen(80);
